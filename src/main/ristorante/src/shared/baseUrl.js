07513900c77ffef3920e37095828c60017879bf9
thegreatone9 <musashakib123@gmail.com>
2020-09-06 02:12:39 +0600

    fixing favicon and port number issues

--- src/main/ristorante/src/shared/baseUrl.js
+++ src/main/ristorante/src/shared/baseUrl.js
@@ -2,4 +2,4 @@
 
 //this is the single place where we can update the server address and use this const elsewhere in our app
 //export const baseUrl = 'http://localhost:3001/';
-export const baseUrl = 'http://localhost:5000/';
+export const baseUrl = 'http://localhost:' + location.port +'/';
